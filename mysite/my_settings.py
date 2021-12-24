097947fdc70899c6289a0d68b2ecd0fe16527cf2
CasselKim <seongr39@gmail.com>
2021-12-24 17:54:35 +0900

    Update Remains

--- mysite/my_settings.py
+++ mysite/my_settings.py
@@ -2,10 +2,11 @@ import sys
 import json
 import os
 
-BASE_DIR = ...
-ROOT_DIR = os.path.dirname(BASE_DIR)
-SECRETS_PATH = os.path.join(ROOT_DIR, 'secret.join')
-secrets = json.loads(open(SECRETS_PATH).read())
+#BASE_DIR = ...
+#print(BASE_DIR)
+#ROOT_DIR = os.path.dirname(BASE_DIR)
+#SECRETS_PATH = os.path.join(ROOT_DIR, 'secret.join')
+secrets = json.loads(open('secret.json').read())
 
 for key, value in secrets.items():
     setattr(sys.modules[__name__], key, value)
